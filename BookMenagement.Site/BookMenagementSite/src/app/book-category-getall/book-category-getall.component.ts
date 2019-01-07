b460af389cf71688815a346c591d3f2444fe5bbe
Julian Tabaku <julian.tabaku@ikubinfo.al>
2019-01-07 17:58:20 +0100

    create book-category-Getall component

---
+++ BookMenagement.Site/BookMenagementSite/src/app/book-category-getall/book-category-getall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-category-getall',
+  templateUrl: './book-category-getall.component.html',
+  styleUrls: ['./book-category-getall.component.css']
+})
+export class BookCategoryGetallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
